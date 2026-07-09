73bb18562ef25b95bf482a6c9c2966624f093582
Humayra Binte Kazal <humayrabintekazal@gmail.com>
2026-07-09 14:01:26 +0600

    Initial project setup with Transaction model

---
+++ Finance Tracker/Form1.cs
@@ -0,0 +1,10 @@
+namespace Finance_Tracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Finance Tracker/Transaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Finance_Tracker
+{
+    internal class Transaction
+    {
+    }
+}
